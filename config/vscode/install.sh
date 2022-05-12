988cf7557bd15b53f2b64e48a81df4aa6d026d60
teaglebuilt <dillan.teagle.va@gmail.com>
2022-05-12 07:06:10 -0500

    setup work

--- config/vscode/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-EXTENSIONS="./extensions.txt"
-while read EXTENSIONS
-do
-    fancy_echo "Install VSCode Extension: $EXTENSION"
-    code --install-extension $EXTENSION
-done < $HOME/.config/vscode/extensions.txt
-
